93d32bfa704f697514812f4be807bfcd33f33e71
hayheh <127457351+hayheh@users.noreply.github.com>
2023-11-16 20:24:44 -0300

    1611

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-  function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
 }
+ const listaDeTeclas = Document.querySelectorAll('.tecla'); 
 
-document.querySelector('.tecla')
+//para
+ for(let contador = 0; contador <listaDeTeclas.length;contador++) {
+
+   const tecla = listaDeTeclas{contador};
+    const instrumento = tecla.ClassList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+        
+   tecla.onclick = function () { 
+       tocaSom(idAudio);
+    } 
+
+    tecla.onkeydown = function (evento) {
+
+    console.log(evento.cod== 'Space')
+
+       if(evento.code == 'Space'){
+
+      tecla.ClassList.add('ativa'); 
+}
+    tecla.onkeydown = function() {
+         tecla.ClassList.remove('ativa'); }
+
+
